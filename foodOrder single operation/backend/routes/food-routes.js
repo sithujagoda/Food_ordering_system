f6fa843a1e88be3e07f07d8a9b812c926a1abf76
sithuminijagoda <sithuminikjagoda@gmail.com>
2025-05-23 15:01:08 +0530

    Deleted frontend folder

--- foodOrder single operation/backend/routes/food-routes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { getAllFoods } = require("../controllers/food-controller");
-
-const router = express.Router();
-
-router.get("/get/:shopId", getAllFoods);
-
-module.exports = router;
--- foodOrder single operation/backend/routes/shop-routes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { getAllShops } = require("../controllers/shop-controller");
-
-const router = express.Router();
-
-router.get("/get", getAllShops);
-
-module.exports = router;
